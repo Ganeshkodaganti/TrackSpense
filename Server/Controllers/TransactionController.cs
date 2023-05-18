8b676bb6a9398fd7626405bd79eb215c23ae8d2d
ganesh.kodaganti <ganesh.kodaganti@TGU1DELL0119>
2023-05-18 14:51:32 +0530

    Add project files.

---
+++ Server/Controllers/TransactionController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrackSpense.BL.Contracts;
+using TrackSpense.Shared.BusinessModels;
+
+namespace TrackSpense.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController:Controller
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        //add a new transaction
+        [HttpPost("AddTransaction")]
+        public async Task<Business_Transaction> Post(Business_Transaction transaction)
+        {
+            transaction.TransactionDate = DateTime.Now;
+            return await _transactionService.Add(transaction);
+        }
+
+        //get all transactions
+        [HttpGet]
+        public async Task<List<Business_Transaction>> Get(string UserId)
+        {
+            return await _transactionService.GetAll(UserId);
+        }
+
+        //Delete Transaction
+        [HttpDelete]
+        public async Task<Business_Transaction> Delete(Business_Transaction transaction)
+        {
+            return await _transactionService.Delete(transaction);
+        }
+
+        //Update Trasaction
+        [HttpPut]
+        public async Task<Business_Transaction> Update(Business_Transaction transaction)
+        {
+            return await _transactionService.Update(transaction);
+        }
+
+    }
+}
---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TrackSpense.BL.Contracts;
+using TrackSpense.DAL.Models;
+using TrackSpense.Shared.BusinessModels;
+
+namespace TrackSpense.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+    private static IConfiguration _config;
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("Register")]
+    public async Task<bool> RegisterUser([FromBody]Business_User user)
+    {
+        var UsernameExists = await IsUsernameExists(user.UserName);
+        //Encrypting password using BCrpt
+        user.Password=BCrypt.Net.BCrypt.HashPassword(user.Password);
+        if (UsernameExists == false)
+        {
+            await _userService.Add(new Business_User()
+            {
+                UserId = "",
+                UserName = user.UserName,
+                Password = user.Password,
+                Email = user.Email
+            });
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
+
+    [HttpPost("login")]
+    public async Task<Business_User> LoginUser(string UserName,string Password)
+    {
+        var UsernameExists = await IsUsernameExists(UserName);
+        if (UsernameExists == false) return null; 
+
+        return await _userService.Get(UserName,Password);
+
+    }
+
+    [HttpGet("IsUsernameExists")]
+    public async Task<bool> IsUsernameExists(string Username)
+    {
+        return await _userService.GetByUserName(Username);
+    }
+
+
+}
